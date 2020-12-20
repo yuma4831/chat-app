02331a60c0216bb8a033af0d84f7c164fb6500b7
yuma <yuma.4831@gmail.com>
2020-12-20 11:14:20 +0900

    フロント実装の準備

--- config/application.rb
+++ config/application.rb
@@ -10,7 +10,7 @@ module ChatApp
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 6.0
-    config.genarators do |g|
+    config.generators do |g|
       g.sytlesheets false
       g.javascripts false
       g.helper false
