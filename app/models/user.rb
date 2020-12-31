b9407d7afd3e0fd079f17ae023a7be21a8d2ab44
yuma <yuma.4831@gmail.com>
2020-12-31 14:11:55 +0900

    room_usersテーブルの作成

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,10 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-        validates :name, presence: true
+  validates :name, presence: true
 
-        end
+  has_many :room_users
+  has_many :rooms, through: :room_users
+
+end
+
